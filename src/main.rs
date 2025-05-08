88aac2bd07c2adee805440f1e927df07faac75e9
tanmay <tanmaykumar.kanpur@gmail.com>
2025-05-08 23:06:39 +0530

    building

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use chrono::{DateTime, Local, Utc};
+
 fn main() {
-    println!("Hello, world!");
+    let utc: DateTime<Utc> = Utc::now();
+    let local_time: DateTime<Local> = Local::now();
+    println!("current UTC time is: {}", utc);
+    println!("local time is: {}", local_time);
 }

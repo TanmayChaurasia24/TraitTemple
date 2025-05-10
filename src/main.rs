51544574b2c1d5997d9918e5bb6249cac6ff98c7
tanmay <tanmaykumar.kanpur@gmail.com>
2025-05-10 23:29:08 +0530

    trait and struct practice done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-use chrono::{DateTime, Local, Utc};
+
+// traits are the properties that struct wants to be in it. like rect is the struct that has the trait of shape why? because rect has the area function if it does not have this function then rect does not have the trait of shape. similary for circle also.
+trait Shape {
+    fn area(&self) -> u32;
+}
+
+struct Rect {
+    width: u32,
+    height: u32
+}
+
+impl Shape for Rect {
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+}
+
+struct Circle {
+    radius: u32
+}
+
+impl Shape for Circle {
+    fn area(&self) -> u32 {
+        return self.radius * self.radius;
+    }
+}
 
 fn main() {
-    let utc: DateTime<Utc> = Utc::now();
-    let local_time: DateTime<Local> = Local::now();
-    println!("current UTC time is: {}", utc);
-    println!("local time is: {}", local_time);
+    let r: Rect = Rect {
+        width: 10,
+        height: 10
+    };
+
+    let c: Circle = Circle { radius: (5) };
+
+    get_area(r);
+    get_area(c);
+}
+
+// this is the function that only accepts the things that has the trait of shape, rect and circle both has that because both of them has area function in them.
+fn get_area(s: impl Shape) -> u32 {
+    return s.area()
 }
